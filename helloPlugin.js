40ba8494b13d8b9b553e4a35d42c7594e5b1c8dc
bennstorey <benn.storey@theapplab.net>
2025-04-16 21:45:48 +0100

    Create helloPlugin.js

---
+++ helloPlugin.js
@@ -0,0 +1,8 @@
+(function () {
+  window.plugin = {
+    initialize: function () {
+      alert('✅ Plugin is loaded and active!');
+      console.log('✅ Plugin is loaded and active!');
+    }
+  };
+})();
