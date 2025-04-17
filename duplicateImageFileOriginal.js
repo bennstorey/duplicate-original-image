bb604b2711fd324b6c15e81e8e83a1efaae4ed6a
bennstorey <benn.storey@theapplab.net>
2025-04-17 10:15:45 +0100

    Update duplicateImageFileOriginal.js

--- duplicateImageFileOriginal.js
+++ duplicateImageFileOriginal.js
@@ -1,5 +1,5 @@
 (function () {
-  console.log("✅ 004 Plugin: Duplicate Original Image - Dossier Toolbar");
+  console.log("✅ 005 Plugin: Duplicate Original Image - Dossier Toolbar");
 
   function waitForContentStationSdk(callback) {
     if (typeof window.ContentStationSdk !== "undefined") {
