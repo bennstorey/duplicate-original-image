31b46e5b7c1aef6fbb4ad4ba664e5b70444476af
bennstorey <benn.storey@theapplab.net>
2025-04-18 18:21:59 +0100

    Update duplicateImageFileOriginal.js

--- duplicateImageFileOriginal.js
+++ duplicateImageFileOriginal.js
@@ -1,5 +1,5 @@
 (function () {
-  console.log("✅ A84 Plugin: Duplicate Original Image - Dossier Button");
+  console.log("✅ A84 Plugin: Duplicate Original Image - Dossier Button"); 
 
   let sessionInfo = null;
 
