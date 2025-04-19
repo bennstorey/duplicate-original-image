2c75714727953c09597f59f63940a1e5b8f4754b
bennstorey <benn.storey@theapplab.net>
2025-04-19 07:28:43 +0100

    Update duplicateImageFileOriginal.js

--- duplicateImageFileOriginal.js
+++ duplicateImageFileOriginal.js
@@ -1,5 +1,5 @@
 (function () {
-  console.log("✅ B10 Plugin: Duplicate Original Image - Dossier Button");
+  console.log("✅ B11 Plugin: Duplicate Original Image - Dossier Button");
 
   let sessionInfo = null;
 
@@ -132,7 +132,8 @@
                 Dossier: { ID: dossier.ID },
                 ContentMetaData: {
                   ContentPath: uploadJson.Path
-                }
+                },
+                Metadata: meta.Object.Metadata || {}
               }
             ]
           };
